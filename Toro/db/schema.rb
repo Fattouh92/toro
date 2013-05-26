1e077895615dc7ea4e5db68a0ea64ae176efd878
Mohamed Tamer Aboul Azm <m-fattouh@hotmail.com>
2013-05-26 15:22:36 +0200

    devise now uses username and email isnt required, errors are shown in a good way, password min length is 6 and first name and last are obtained

--- Toro/db/schema.rb
+++ Toro/db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130523163258) do
+ActiveRecord::Schema.define(:version => 20130526124347) do
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -24,8 +24,12 @@ ActiveRecord::Schema.define(:version => 20130523163258) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+    t.string   "username"
+    t.string   "firstname"
+    t.string   "lastname"
+    t.boolean  "admin",                  :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
