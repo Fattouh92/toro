6dfc4ed07de3e01fadeba675aa3efcfec6f3dfc2
Omar Hossam <omar.csis101@gmail.com>
2013-05-31 21:46:38 +0200

    check if table has no running cheque

--- Toro/app/models/check.rb
+++ Toro/app/models/check.rb
@@ -1,5 +1,9 @@
 class Check < ActiveRecord::Base
-  attr_accessible :datetime, :posttax, :pretax, :taxrate
+  attr_accessible :datetime, :posttax, :pretax, :taxrate, :current
   has_many :items 
   belongs_to :table
+
+  def find_by_table_number(table_number)
+  	Check.where(table_id: Table.where(number: table_number).id)
+  end
 end
