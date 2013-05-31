9d6eef4ded2d50bd76cbea0ad4d5982f34f424fa
mostafahassaan <mostafa.a.hassaan@gmail.com>
2013-05-31 17:50:27 +0200

    modified table

--- Toro/app/controllers/table_controller.rb
+++ Toro/app/controllers/table_controller.rb
@@ -5,8 +5,8 @@ class TableController < ApplicationController
     @tables = Table.all
   end
 
-  def add_table(table_number)
-    @table = Table.new(number: params[:number])
+  def add_table(table_number, placement)
+    @table = Table.new(number: params[:number], placement: params[:placement])
   end
 
   def remove_table(table_id)
