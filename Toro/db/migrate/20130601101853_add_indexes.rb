9722bd87b176a0bf67025416b4b38f0a4ce1f82f
Omar Hossam <omar.csis101@gmail.com>
2013-06-01 16:04:59 +0200

    modify db

--- Toro/db/migrate/20130601101853_add_indexes.rb
+++
@@ -1,9 +0,0 @@
-class AddIndexes < ActiveRecord::Migration
-  def up
-    add_column :items, :category_id, :integer
-    change_column :items, :category_id, :integer, :null => false
-  end
-
-  def down
-  end
-end
