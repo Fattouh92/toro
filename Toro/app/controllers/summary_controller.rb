36949e4bc47f79b4321dae481cfdbcb70d6fd454
mostafahassaan <mostafa.a.hassaan@gmail.com>
2013-06-07 04:52:26 +0200

    added more filters

--- Toro/app/controllers/summary_controller.rb
+++ Toro/app/controllers/summary_controller.rb
@@ -14,21 +14,48 @@ class SummaryController < ApplicationController
 
   def filter
     date = params[:summary][:date]
+    date2 = params[:summary][:date2]
     shift = params[:shift]
-    if (Torodate.where(id: date).last.nil?)
+    if (Torodate.where(id: date).last.nil? && Torodate.where(id: date2).last.nil?)
       flash[:error] = "Date must be chosen."
       redirect_to summary_path
       return
     end
-    trueDate = Torodate.where(id: date).last.date
-    
-    if (shift == "")
-      @check = Check.where(date: trueDate).all
-      render "filter"
-    else
-      @check = Check.where(date: trueDate, shift: shift).all
-      render "filter"
-    end
-  end
 
+    if (!Torodate.where(id: date).last.nil? && Torodate.where(id: date2).last.nil?)
+      trueDate = Torodate.where(id: date).last.date
+      if (shift == "")
+        @check = Check.where(date: trueDate).all
+        render "filter"
+      else
+        @check = Check.where(date: trueDate, shift: shift).all
+        render "filter"
+      end
+    end
+
+    if (Torodate.where(id: date).last.nil? && !Torodate.where(id: date2).last.nil?)
+      trueDate = Torodate.where(id: date2).last.date
+      if (shift == "")
+        @check = Check.where(date: trueDate).all
+        render "filter"
+      else
+        @check = Check.where(date: trueDate, shift: shift).all
+        render "filter"
+      end
+    end
+
+    if (!Torodate.where(id: date).last.nil? && !Torodate.where(id: date2).last.nil?)
+      trueDate = Torodate.where(id: date).last.date
+      trueDate2 = Torodate.where(id: date2).last.date
+      if (shift == "")
+        @check = Check.where('date <= ?', trueDate).where('date >= ?', trueDate2).all
+        render "filter"
+      else
+        @check = Check.where('date <= ?', trueDate).where('date >= ?', trueDate2)
+          .where(shift: shift).all
+        render "filter"
+      end
+    end
+
+  end
 end
