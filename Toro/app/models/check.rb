4e61c1a2d7006d536332ceaa4e095eac41e2fd2e
Omar Hossam <omar.csis101@gmail.com>
2013-06-09 00:21:04 +0200

    modifications

--- Toro/app/models/check.rb
+++ Toro/app/models/check.rb
@@ -2,7 +2,7 @@ class Check < ActiveRecord::Base
   attr_accessible :datetime, :taxrate, :current, :number_of_customers, :min_charge, :sum, :table_id, :payment_method, :name
   has_many :orders 
   belongs_to :table
-  validates_presence_of :date, :shift, :table_id
+  validates_presence_of :date, :shift, :table_id, :number_of_customers, :captain_id
   def cheques
   	if self.name == nil
   		' ' + Table.find(self.table_id).placement + Table.find(self.table_id).number.to_s
